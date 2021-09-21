ad6d256283bdd1d78adce5ca7fd27a4fd7fd539a
Thomas Rasch <thomas.rasch@outdooractive.com>
2021-09-21 14:56:51 +0200

    Dependency update (RTree)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["MVTTools"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Outdooractive/gis-tools", from: "0.2.5"),
+        .package(url: "https://github.com/Outdooractive/gis-tools", from: "0.2.7"),
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf", from: "1.17.0"),
     ],
     targets: [
