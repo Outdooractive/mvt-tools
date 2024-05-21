d195d9db04831470c6551533bc5017ee60b92d7c
Thomas Rasch <thomas.rasch@outdooractive.com>
2024-05-21 16:54:16 +0200

    Check for /usr/local/bin before installing the command line tool

--- install_mvt.sh
+++ install_mvt.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-swift build -c release --arch arm64 --arch x86_64 --product mvt >/dev/null
+echo "Building the 'mvt' command line tool..."
+
+swift build --quiet -c release --arch arm64 --arch x86_64 --product mvt 2>&1 >/dev/null
 if [[ $? -ne 0 ]]
 then
     echo "Failed to build 'mvt'"
     exit 1
+fi
+
+if [[ ! -d "/usr/local/bin" ]]
+then
+    echo "/usr/local/bin doesn't exist, trying to create it (with sudo)"
+    sudo mkdir -p /usr/local/bin || exit 1
 fi
 
 cp .build/apple/Products/Release/mvt /usr/local/bin/
