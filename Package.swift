e8651d6b74a370347cf11852f650a29bf2b5f895
Thomas Rasch <thomas.rasch@outdooractive.com>
2024-09-11 19:38:19 +0200

    Revert GzipSwift to 5.2.0

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Outdooractive/gis-tools", from: "1.8.4"),
-        .package(url: "https://github.com/1024jp/GzipSwift.git", from: "6.1.0"),
+        .package(url: "https://github.com/1024jp/GzipSwift.git", from: "5.2.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.1"),
         .package(url: "https://github.com/apple/swift-protobuf", from: "1.26.1"),
