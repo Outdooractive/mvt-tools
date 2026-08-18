f186a22c38f2dc9740e6dd98be7a71cbe168d38d
Thomas Rasch <thomas.rasch@outdooractive.com>
2026-08-18 12:21:02 +0200

    Dependency updates

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(url: "https://github.com/Outdooractive/gis-tools-csv", from: "1.1.0"),
         .package(url: "https://github.com/Outdooractive/gis-tools-geopackage", from: "1.0.2"),
         .package(url: "https://github.com/Outdooractive/gis-tools-gpx", from: "1.0.4"),
-        .package(url: "https://github.com/Outdooractive/gis-tools-fit", from: "1.0.0"),
+        .package(url: "https://github.com/Outdooractive/gis-tools-fit", from: "1.1.4"),
         .package(url: "https://github.com/Outdooractive/gis-tools-shapefile", from: "1.0.2"),
         .package(url: "https://github.com/1024jp/GzipSwift.git", from: "7.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.8.2"),
